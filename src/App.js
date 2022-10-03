3b004246a22b4f577c0691424c49a488b9e36bf1
AnnaKoziol1997 <85227390+AnnaKoziol1997@users.noreply.github.com>
2022-10-03 12:02:19 +0200

    Pokazywanie zdjec

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 //import './App.css';
 import React from 'react';
-import Title from './comps/Title';
-import UploadForm from './comps/UploadForm';
+import {useState,useEffect} from 'react';
+import {storage} from './firebase';
+import {ref,uploadBytes, getDownloadURL,listAll,list} from 'firebase/storage';
+import{v4} from 'uuid'
+import Title from './Title';
 
-function App() {
+function App() 
+{ 
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const imagesListRef = ref(storage, "images/");
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
+  useEffect(() => {
+    listAll(imagesListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setImageUrls((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
+      <div className='Title'>
       <Title/>
-      <UploadForm/>
+      </div>
+      <div className='UploadAndImg'>
+        <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+        />
+        <button onClick={uploadFile}> Upload Image</button>
+        {imageUrls.map((url) => {
+          return <img src={url} />;
+        })} 
+      </div>
+      
     </div>
   );
 }
-
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getStorage}from"firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9gwCCyYAHU9JdmYQzXm8b_nZIbk-OalU",
+    authDomain: "foodgallery-11f09.firebaseapp.com",
+    projectId: "foodgallery-11f09",
+    storageBucket: "foodgallery-11f09.appspot.com",
+    messagingSenderId: "763261753415",
+    appId: "1:763261753415:web:23d2d57496e70fe2063962"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
