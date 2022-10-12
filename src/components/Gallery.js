7425dbae9b09e9c1ac88d2c2f2d8732ff7873167
AnnaKoziol1997 <85227390+AnnaKoziol1997@users.noreply.github.com>
2022-10-12 11:10:58 +0200

    male zmiany

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { storage } from '../firebase'
-import { ref, getDownloadURL, listAll } from 'firebase/storage';
-//import { v4 } from 'uuid'
-import uploadFile from '../files/uploadFile';
+import { ref, getDownloadURL, uploadBytes, listAll } from 'firebase/storage';
+import { v4 } from 'uuid'
+//import uploadFile from '../files/uploadFile';
 
 const Gallery = ()=>{
-    const [/*imageUpload*/, setImageUpload] = useState(null);
+    const [imageUpload, setImageUpload] = useState(null);
   const [imageUrls, setImageUrls] = useState([]);
 
   
- // const uploadFile = () => {
- //   if (imageUpload == null) return;
- //   const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
- //   uploadBytes(imageRef, imageUpload).then((snapshot) => {
- //     getDownloadURL(snapshot.ref).then((url) => {
- //       setImageUrls((prev) => [...prev, url]);
- //     });
- //   });
- // };
+  const uploadFile = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageUrls((prev) => [...prev, url]);
+      });
+    });
+  };
   
   useEffect(() => {
     const imagesListRef = ref(storage, "images/");
